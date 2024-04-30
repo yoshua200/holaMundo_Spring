e318fc5682571a0741679a22fd948d8e2698a8f0
yoshua200 <yoshua200@gmail.com>
2024-04-29 20:46:02 -0600

    Creando proyecto

---
+++ src/test/java/com/mx/Hola/HolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.Hola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
